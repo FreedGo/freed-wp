24ccedde83f4941cba4d19b81b89784409a9ab85
Freed <flyxz@126.com>
2017-08-17 13:45:20 +0800

    修改配置

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'fly200816');
+define('DB_PASSWORD', '');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
